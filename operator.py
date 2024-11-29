db89021c4492a99cee61134b3eacc461c4e204ae
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-11-28 22:24:54 -0300

    Operators

---
+++ operator.py
@@ -0,0 +1,8 @@
+print(5 * 5)
+print(5 + 5)
+print(5 - 5)
+print(10 / 5)
+print(10 // 5)
+print(5 % 2)
+print(5 ** 3)
+ # type: ignore
--- type.py
+++ type.py
@@ -1,3 +1,6 @@
 print("hello world")
 
 print(type("hello world"))
+print(type(3.2))
+print(type(3 + 2))
+
